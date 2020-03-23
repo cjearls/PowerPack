ea25697428d1c9cf8050c06c761f82902a19abf4
powerpack <powerpack@localhost.localdomain>
2020-03-23 19:22:31 -0400

    Update graphing script

--- plot_readings.py
+++ plot_readings.py
@@ -32,7 +32,7 @@ with open(sys.argv[1], 'r') as f:
     # process timestamps
     for i in range(num_timestamps):
         line = f.readline()
-        time, message = line.split('\t')
+        time, message = line.split('|')
         time = (float(time) - start_time) / 1000000000
         timestamps.append({'time': time, 'message': message})
 
@@ -41,7 +41,7 @@ with open(sys.argv[1], 'r') as f:
     # process power readings
     for i in range(math.ceil(num_samples/sample_rate)):
         line = f.readline()
-        data = [float(measurement.strip()) for measurement in line.split()]
+        data = [float(measurement.strip()) for measurement in line.split(',')]
         raw_data.append(data)
 
         # - time is calculated based on readings index relative to the
@@ -51,7 +51,8 @@ with open(sys.argv[1], 'r') as f:
             {'time': sample_rate/samp_per_second * (i+1),
             'power': sum(data[-6::])})
 
-
+x_max = max(timestamps, key=lambda x: x['time'])['time'] 
+y_max = max(readings, key=lambda x: x['power'])['power'] 
 
 # Create main canvas and add subplots
 fig = plt.figure()
@@ -60,7 +61,7 @@ ax = fig.add_subplot(111)
 # add vertical lines for timestamps
 for timestamp in timestamps:
     ax.axvline(x=timestamp['time'], linestyle='--')
-    ax.text(timestamp['time'], random.randint(240,320), timestamp['message'], fontsize=6)
+    ax.text(timestamp['time'], random.randint(0,int(y_max)), timestamp['message'], fontsize=6)
 
 # plot values
 ax.plot([i['time'] for i in readings], [i['power'] for i in readings], 'b', linestyle='-')
@@ -71,7 +72,7 @@ ax.set_xlabel('Time (seconds)')
 ax.set_ylabel('Power (joules)')
 
 # set axis scale
-ax.axis([ 0, max(timestamps, key=lambda x: x['time'])['time'] + 1, 0, max(readings, key=lambda x: x['power'])['power'] + 10])
+ax.axis([ 0, x_max + 1, 0, y_max + 10])
 
 
 # print(readings)

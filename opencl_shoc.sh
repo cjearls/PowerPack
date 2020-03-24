49771fd7ab519eb1a1d7b73d9cfe6a3707c97cc4
powerpack <powerpack@localhost.localdomain>
2020-03-24 14:21:26 -0400

    updated configs

--- opencl_shoc.sh
+++ opencl_shoc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-./configure CPPFLAGS="-I/usr/local/cuda-10.2/include" LDFLAGS="-L/usr/local/cuda-10.2/targets/x86_64-linux/lib/ -lOpenCL" --with-opencl
+#./configure CPPFLAGS="-I/usr/local/cuda-10.2/include" LDFLAGS="-L/usr/local/cuda-10.2/targets/x86_64-linux/lib/ -lOpenCL" --with-opencl
+./configure LDFLAGS="-L/home/powerpack/intel/system_studio_2020/opencl/SDL/lib64" --with-opencl
 
